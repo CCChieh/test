233a1e879091764afc5667ffc138cc25baa90890
Chieh <897651818@qq.com>
2019-08-13 15:47:07 +0800

    rest api.

--- logger.go
+++ logger.go
@@ -9,9 +9,11 @@ import (
 func Logger(inner http.Handler, name string) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now()
+
 		inner.ServeHTTP(w, r)
 
-		log.Printf("%s\t%s\t%s\t%s",
+		log.Printf(
+			"%s\t%s\t%s\t%s",
 			r.Method,
 			r.RequestURI,
 			name,
--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 )
 
 func main() {
+
 	router := NewRouter()
+
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
