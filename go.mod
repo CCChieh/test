2229d42f342dba3cca8c6b8033612e8b73dcab08
Chieh <897651818@qq.com>
2019-08-21 03:02:14 +0800

    JWT test

--- go.mod
+++ go.mod
@@ -3,6 +3,9 @@ module test
 go 1.12
 
 require (
-	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/SermoDigital/jose v0.9.1 // indirect
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/gbrlsnchs/jwt v1.1.0 // indirect
+	github.com/gbrlsnchs/jwt/v3 v3.0.0-beta.1
 	github.com/gorilla/mux v1.7.3
 )
--- main.go
+++ main.go
@@ -1,10 +1,46 @@
 package main
 
 import (
-	"encoding/json"
-	"net/http"
+	"fmt"
+	"github.com/gbrlsnchs/jwt/v3"
+	"time"
 )
 
-func main() {
+type CustomPayload struct {
+	jwt.Payload
+	Foo string `json:"foo,omitempty"`
+	Bar int    `json:"bar,omitempty"`
+}
 
+var hs = jwt.NewHS256([]byte("secret"))
+
+func main() {
+	now := time.Now()
+	pl := CustomPayload{
+		Payload: jwt.Payload{
+			Issuer:         "gbrlsnchs",
+			Subject:        "someone",
+			Audience:       jwt.Audience{"https://golang.org", "https://jwt.io"},
+			ExpirationTime: jwt.NumericDate(now.Add(24 * 30 * 12 * time.Hour)),
+			NotBefore:      jwt.NumericDate(now.Add(30 * time.Minute)),
+			IssuedAt:       jwt.NumericDate(now),
+			JWTID:          "foobar",
+		},
+		Foo: "foo",
+		Bar: 1337,
+	}
+
+	token, err := jwt.Sign(pl, hs)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(string(token))
+	fmt.Println("-----------")
+
+	hd, err := jwt.Verify(token, hs, &pl)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(hd.Algorithm, hd.ContentType, hd.KeyID, hd.Type)
 }
